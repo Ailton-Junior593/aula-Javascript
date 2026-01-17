b9aec9392fbda3b1c03f3836a78996c0b84bb846
Ailton Junior <adsjr01@gmail.com>
2026-01-17 18:47:22 -0300

    Primeira Aula de JavaScript

---
+++ aula01/main.js
@@ -0,0 +1,67 @@
+// alert('Hello World') // alerta
+// console.log('Hello Word 2') // semelhante ao print no python
+
+// variaveis
+
+// var  nome = 'Felipe'// string ou str
+// let  nome2 = 'João'// string ou str
+// const SENHA = '12345'// string ou str
+
+// var idade = 25 // int ou integer
+
+// var altura = 1.90 // float
+
+// var online = true // boolean
+
+// // console.log('Nome: ', nome);
+// // console.log('Senha:', SENHA);
+
+// var nome3 = 'Ailton'
+// alert(nome3);
+
+// let nome = prompt('Digite o seu nome :')
+// alert(nome)
+
+// let continuar = confirm('Deseja continuar?')
+
+// console.log(typeof{idade})
+
+// idade = parseInt(idade)
+
+// console.log(typeof(idade))
+
+
+// operadores
+
+// let numero1 = 3
+// let numero2 = 10
+
+// // console.log(numero1 + numero2);
+// // console.log(numero1 - numero2);
+// // console.log(numero1 / numero2);
+// // console.log(numero1 * numero2);
+// // console.log(numero1 % numero2);
+
+
+// // numero1 += numero1
+// // numero1 *= numero1
+// // numero1 /= numero1
+// // numero1 -= numero1
+
+// // console.log(numero1)
+
+
+// let numero1 = prompt('Digite o Primeiro numero:');
+// let numero2 = prompt('Digite o Segundo numero:');
+
+
+// var soma = parseInt(numero1) + parseInt(numero2);
+// alert(soma)
+// var subtrair = numero1 - numero2;
+// alert(subtrair);
+// var multiplicar = numero1 * numero2;
+// alert(multiplicar);
+// var divisao = numero1 / numero2;
+// alert(divisao);
+
+
